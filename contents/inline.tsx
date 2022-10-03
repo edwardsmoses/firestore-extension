bdb00f6e236c64083ba59ba65a0403393360c20b
Edwards Moses <edwardsmoses3@gmail.com>
2022-10-03 01:58:35 +0100

    added content ui to every field in firestore

--- contents/inline.tsx
+++ contents/inline.tsx
@@ -3,14 +3,36 @@ import type {
   PlasmoGetInlineAnchor,
   PlasmoMountShadowHost
 } from "plasmo"
+import { createRoot } from "react-dom/client"
 
 export const config: PlasmoContentScript = {
   matches: ["https://console.firebase.google.com/*"]
 }
 
 export const getInlineAnchor: PlasmoGetInlineAnchor = () => {
-  console.log("are you called", document.querySelector("span.database-leaf-value.ng-star-inserted"));
-  return document.querySelector("span.database-leaf-value.ng-star-inserted")
+  console.log(
+    "are you called",
+    document.querySelectorAll("span.database-leaf-value.ng-star-inserted")
+  )
+
+  const fieldValueSpans = document.querySelectorAll(
+    "span.database-leaf-value.ng-star-inserted"
+  );
+
+  fieldValueSpans.forEach((fieldValue) => {
+    if (!fieldValue.querySelector("#navigator-btn")) {
+      const sendButton = fieldValue.childNodes.item(0)
+
+      const container = document.createElement("div")
+      container.id = "navigator-btn"
+      sendButton?.parentNode?.insertBefore(container, sendButton.nextSibling)
+
+      let root = createRoot(container)
+      root.render(<IdInline />)
+    }
+  })
+
+  return null;
 }
 
 export const mountShadowHost: PlasmoMountShadowHost = ({

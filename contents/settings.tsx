eabc882283ab577f97de1d8e1f79504f926f16af
Edwards Moses <edwardsmoses3@gmail.com>
2022-12-25 00:24:56 +0100

    feat: progress in saving the linked collection to browser storage

--- contents/settings.tsx
+++ contents/settings.tsx
@@ -2,6 +2,9 @@ import { Box, Button, DropButton, FormField, Grommet, TextInput } from "grommet"
 import type { PlasmoContentScript } from "plasmo"
 import { useState } from "react"
 
+import { Storage } from "@plasmohq/storage"
+import { useStorage } from "@plasmohq/storage/hook"
+
 export const config: PlasmoContentScript = {
   matches: ["https://console.firebase.google.com/*"]
 }
@@ -19,17 +22,44 @@ export const getInlineAnchorList = async () => {
 export const getShadowHostId = () => "plasmo-inline-settings-id"
 
 const PlasmoInline = () => {
+  //TODO Temp values...
+  const PROJECT_NAME = "edwards"
+  const FIELD_NAME = "userId"
+
+  const [targetOptions] = useStorage(`${PROJECT_NAME}_${FIELD_NAME}`)
+  console.log("options saved", targetOptions)
+
   return (
     <Grommet>
-      <DropButton primary label="Settings" dropContent={<SettingsBox />} />
+      <DropButton
+        primary
+        label="Settings"
+        dropContent={
+          <SettingsBox fieldName={FIELD_NAME} projectName={PROJECT_NAME} />
+        }
+      />
     </Grommet>
   )
 }
 
-const SettingsBox = () => {
-  
+type SettingsProps = {
+  projectName: string
+  fieldName: string
+}
+const SettingsBox = ({ projectName, fieldName }: SettingsProps) => {
   const [targetCollection, setTargetCollection] = useState("")
   const [emojiIcon, setEmojiIcon] = useState("")
+
+  const handleSave = async () => {
+    const storage = new Storage()
+
+    await storage.set(`${projectName}_${fieldName}`, [
+      {
+        target: targetCollection,
+        icon: emojiIcon
+      }
+    ])
+  }
 
   return (
     <Box pad="xlarge" background="light-2">
@@ -48,7 +78,7 @@ const SettingsBox = () => {
           onChange={(event) => setTargetCollection(event.target.value)}
         />
       </FormField>
-      <Button primary label="Save" />
+      <Button primary label="Save" onClick={handleSave} />
     </Box>
   )
 }

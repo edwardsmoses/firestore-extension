06f405774add499f686eb27c72fbde5c7f5a95da
Edwards Moses <edwardsmoses3@gmail.com>
2022-12-25 22:29:12 +0100

    use the document name when saving the storage info. chore: add types

--- contents/settings.tsx
+++ contents/settings.tsx
@@ -1,7 +1,6 @@
 import { Box, Button, DropButton, FormField, Grommet, TextInput } from "grommet"
-import type { PlasmoContentScript, PlasmoRender } from "plasmo"
-import { useRef, useState } from "react"
-import { createRoot } from "react-dom/client"
+import type { PlasmoContentScript } from "plasmo"
+import { useState } from "react"
 
 import { Storage } from "@plasmohq/storage"
 import { useStorage } from "@plasmohq/storage/hook"
@@ -21,6 +20,24 @@ export const getInlineAnchorList = async () => {
 // Use this to optimize unmount lookups
 export const getShadowHostId = () => "plasmo-inline-settings-id"
 
+type TargetCollection = {
+  target: string
+  icon: string
+}
+
+type StorageKeyArgs = {
+  projectId: string
+  documentName: string
+  fieldName: string
+}
+const generateStorageKey = ({
+  projectId,
+  documentName,
+  fieldName
+}: StorageKeyArgs) => {
+  return `${projectId}_${documentName}_${fieldName}`
+}
+
 const PlasmoInline = (props) => {
   const { anchor } = props
 
@@ -33,18 +50,26 @@ const PlasmoInline = (props) => {
       ?.textContent || ""
   ).replace(/['"]/g, "")
 
-  console.log("field info", fieldKey, fieldValue)
-
-  //TODO Temp values...
-  const PROJECT_NAME = "edwards"
-  const FIELD_NAME = "userId"
-
   //get the current project from the URL
   const currentURL = window.location.href
-  const currentProject = currentURL.split("/")[6]
 
-  const [targetOptions] = useStorage(`${PROJECT_NAME}_${FIELD_NAME}`)
-  console.log("saved options", targetOptions)
+  const urlArray = currentURL.split("/")
+
+  const currentProject = urlArray[6] || ""
+
+  //TODO Look into if there's another way to get the document info from the url
+  const currentDocument = urlArray[urlArray.length - 1]
+    .split("~")[1]
+    .replace("2F", "")
+
+  const storage = useStorage(
+    generateStorageKey({
+      documentName: currentDocument,
+      fieldName: fieldKey,
+      projectId: currentProject
+    })
+  )
+  const targetOptions = storage[0] as TargetCollection[]
 
   return (
     <Grommet>
@@ -63,7 +88,11 @@ const PlasmoInline = (props) => {
           primary
           label="Settings"
           dropContent={
-            <SettingsBox fieldName={FIELD_NAME} projectName={PROJECT_NAME} />
+            <SettingsBox
+              fieldName={fieldKey}
+              documentName={currentDocument}
+              projectId={currentProject}
+            />
           }
         />
       </div>
@@ -71,23 +100,25 @@ const PlasmoInline = (props) => {
   )
 }
 
-type SettingsProps = {
-  projectName: string
-  fieldName: string
-}
-const SettingsBox = ({ projectName, fieldName }: SettingsProps) => {
+type SettingsProps = StorageKeyArgs & {}
+const SettingsBox = ({ documentName, fieldName, projectId }: SettingsProps) => {
   const [targetCollection, setTargetCollection] = useState("")
   const [emojiIcon, setEmojiIcon] = useState("")
 
   const handleSave = async () => {
     const storage = new Storage()
 
-    await storage.set(`${projectName}_${fieldName}`, [
-      {
-        target: targetCollection,
-        icon: emojiIcon
-      }
-    ])
+    const storageKey = generateStorageKey({
+      documentName,
+      fieldName,
+      projectId
+    })
+    const target: TargetCollection = {
+      target: targetCollection,
+      icon: emojiIcon
+    }
+
+    await storage.set(storageKey, [target])
   }
 
   return (

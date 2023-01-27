cdcebc2c7ceda2f043e006241b209d91ab096756
Edwards Moses <edwardsmoses3@gmail.com>
2023-01-27 20:44:20 +0100

    feat: add Close btn to close the emoji picker

--- contents/settings.tsx
+++ contents/settings.tsx
@@ -242,11 +242,41 @@ const SettingsBox = ({ documentName, fieldName, projectId }: SettingsProps) => {
   display: none !important;
 }
 
+.EmojiPickerReact {
+  margin: 15px 0;
+}
+
+.closeEmojiPickerBtn {
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 20px;
+  top: 10px;
+  cursor: pointer;
+}
+
           `}
       </style>
 
       {isEmojiPickerOpen && (
         <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            onClick={() => {
+              setIsEmojiPickerOpen(false)
+            }}
+            className="closeEmojiPickerBtn">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+
           <EmojiPicker
             width="250px"
             height="250px"

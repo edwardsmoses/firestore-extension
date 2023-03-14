60376e5bea6a1bffcab952234413ee5d75b14116
Edwards Moses <edwardsmoses3@gmail.com>
2023-03-14 22:55:54 +0100

    chore: remove dead code.
    initially used to test if the extension was working right.

--- contents/logger.ts
+++ contents/logger.ts
@@ -8,6 +8,4 @@ window.addEventListener("load", () => {
   console.log(
     "You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true."
   )
-
-  document.body.style.background = "pink"
 })
